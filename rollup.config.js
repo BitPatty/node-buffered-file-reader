f78a900fb486794530bbd27f4cb783840153e47a
Matteias Collet <matteias.collet@bluewin.ch>
2022-10-06 13:06:34 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,66 @@
+import pkg from './package.json';
+
+import fs from 'fs';
+import path, { dirname } from 'path';
+
+import typescript from 'rollup-plugin-typescript2';
+import ttypescript from 'ttypescript';
+import keysTransformer from 'ts-transformer-keys/transformer';
+
+const transformers = [
+  (service) => ({
+    before: [keysTransformer(service.getProgram())],
+    after: [],
+  }),
+];
+
+const createPackageJson = {
+  writeBundle: (opts) => {
+    if (!['es', 'cjs'].includes(opts.format) || opts.file === pkg.types) return;
+    const dirName = path.join(__dirname, path.dirname(opts.file));
+    const output = JSON.stringify({
+      type: opts.format === 'es' ? 'module' : 'commonjs',
+    });
+    fs.writeFileSync(path.join(dirName, 'package.json'), output);
+  },
+};
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+        strict: true,
+        compact: false,
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+        strict: true,
+        compact: false,
+      },
+    ],
+    plugins: [
+      typescript({
+        typescript: ttypescript,
+        tsconfig: 'tsconfig.build.json',
+        useTsconfigDeclarationDir: true,
+        transformers,
+        tsconfigOverride: {
+          compilerOptions: {
+            declaration: true,
+            declarationDir: dirname(pkg.types),
+          },
+        },
+      }),
+      createPackageJson,
+    ],
+    external: [],
+  },
+];
