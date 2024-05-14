2bb581c0d35ba7f513e26faf6ed3f8c394b0e4ee
Matteias Collet <matteias.collet@protonmail.ch>
2024-05-14 21:23:42 +0000

    bump version to v2.0.0

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,100 +1,99 @@
 /**
  * The options for the buffered reader
  */
-export type ReaderOptions =
+export type ReaderOptions = {
+  /**
+   * The offset (in bytes) from which to start reading the file.
+   *
+   * If set to a number greater than 0, that number of bytes
+   * will be skipped when reading the first chunk.
+   */
+  startOffset?: number;
+
+  /**
+   * The default chunk size of the reader.
+   *
+   * If provided, the specified number of bytes is read on each
+   * read operation. If no separator is configured, this will
+   * also be the size of the buffer returned on each iteration.
+   *
+   * Note that when the reader reaches the end of the file,
+   * the buffer size may be less than the chunk size.
+   *
+   * Defaults to `100`
+   */
+  chunkSize?: number;
+
+  /**
+   * Whether to fail if the file is modified while its being
+   * processed by the file reader.
+   *
+   * If set to `true`, the file will be tracked for changes
+   * by `watchFile`. If any changes are done to the file
+   * the following read will throw an error.
+   *
+   * See: https://nodejs.org/docs/latest/api/fs.html#fs_fs_watchfile_filename_options_listener
+   *
+   * Note that
+   *
+   * Defaults to `true`.
+   */
+  throwOnFileModification?: boolean;
+
+  /**
+   * The interval of file modification checks in milliseconds.
+   *
+   * The interval specifies at which rate the file stats should
+   * be checked for any modifications.
+   *
+   * Note that the actual detection frequency is still relying
+   * on the NodeJS file watcher.
+   *
+   * Defaults to `1000`
+   */
+  fileModificationPollInterval?: number;
+} & (
   | {
       /**
-       * The offset (in bytes) from which to start reading the file.
+       * The byte pattern to identify the end of a section.
        *
-       * If set to a number greater than 0, that number of bytes
-       * will be skipped when reading the first chunk.
+       * If provided, the reader continues to read bytes into the buffer
+       * until it encounters the separator and continues reading from
+       * the end of the separator during the next iteration.
+       *
+       * This is especially useful when dealing with text files with
+       * a delimiter (such as newlines). Note that the chunk size
+       * configuration still applies for read operations. However,
+       * the returned buffer size may be lower or higher than the
+       * configured size, depending on where it encounters the
+       * separator pattern.
+       *
+       * Defaults to `undefined`
        */
-      startOffset?: number;
+      separator: Uint8Array;
 
       /**
-       * The default chunk size of the reader.
+       * Whether the encountered separator should be trimmed from
+       * The returned buffer. This configuration is ignored if no
+       * separator is specified.
        *
-       * If provided, the specified number of bytes is read on each
-       * read operation. If no separator is configured, this will
-       * also be the size of the buffer returned on each iteration.
+       * If this is set to true the separator will be removed from
+       * the end of the buffer before being returned as result of
+       * the current iteration, else it is kept in the buffer.
        *
-       * Note that when the reader reaches the end of the file,
-       * the buffer size may be less than the chunk size.
+       * E.g. The file 0x12345678 with the separator 0x56 will return
+       *      0x1234 if this is set to `true`, else it will return
+       *      0x123456.
        *
-       * Defaults to `100`
+       * Defaults to `false`
        */
-      chunkSize?: number;
-
-      /**
-       * Whether to fail if the file is modified while its being
-       * processed by the file reader.
-       *
-       * If set to `true`, the file will be tracked for changes
-       * by `watchFile`. If any changes are done to the file
-       * the following read will throw an error.
-       *
-       * See: https://nodejs.org/docs/latest/api/fs.html#fs_fs_watchfile_filename_options_listener
-       *
-       * Note that
-       *
-       * Defaults to `true`.
-       */
-      throwOnFileModification?: boolean;
-
-      /**
-       * The interval of file modification checks in milliseconds.
-       *
-       * The interval specifies at which rate the file stats should
-       * be checked for any modifications.
-       *
-       * Note that the actual detection frequency is still relying
-       * on the NodeJS file watcher.
-       *
-       * Defaults to `1000`
-       */
-      fileModificationPollInterval?: number;
-    } & (
-      | {
-          /**
-           * The byte pattern to identify the end of a section.
-           *
-           * If provided, the reader continues to read bytes into the buffer
-           * until it encounters the separator and continues reading from
-           * the end of the separator during the next iteration.
-           *
-           * This is especially useful when dealing with text files with
-           * a delimiter (such as newlines). Note that the chunk size
-           * configuration still applies for read operations. However,
-           * the returned buffer size may be lower or higher than the
-           * configured size, depending on where it encounters the
-           * separator pattern.
-           *
-           * Defaults to `undefined`
-           */
-          separator: Uint8Array;
-
-          /**
-           * Whether the encountered separator should be trimmed from
-           * The returned buffer. This configuration is ignored if no
-           * separator is specified.
-           *
-           * If this is set to true the separator will be removed from
-           * the end of the buffer before being returned as result of
-           * the current iteration, else it is kept in the buffer.
-           *
-           * E.g. The file 0x12345678 with the separator 0x56 will return
-           *      0x1234 if this is set to `true`, else it will return
-           *      0x123456.
-           *
-           * Defaults to `false`
-           */
-          trimSeparator?: boolean;
-        }
-      | {
-          separator?: undefined;
-          trimSeparator?: never;
-        }
-    );
+      trimSeparator?: boolean;
+    }
+  | {
+      separator?: undefined;
+      trimSeparator?: never;
+    }
+);
 
 export class Configuration {
   /**
